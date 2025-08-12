50089a58e1d29e259e6cf408eae7cd148767c528
ErbilB <erbilbabacanoglu@gmail.com>
2025-08-12 16:26:56 +0300

    currency converter added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const url = "https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD";
+const baseUrl = "https://api.frankfurter.app/latest?";
 
 function App() {
+  const [amount, setAmount] = useState(50);
+
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("EUR");
+
+  const [result, setResult] = useState("");
+
+  function handleAmountChange(e) {
+    const value = Number(e.target.value);
+    setAmount(isNaN(value) ? 0 : value);
+  }
+
+  useEffect(
+    function () {
+      async function convert() {
+        const response = await fetch(
+          `${baseUrl}amount=${amount}&from=${from}&to=${to}`
+        );
+
+        const data = await response.json();
+
+        const convertResult = data.message
+          ? "Please use different currencies"
+          : data.rates[to];
+
+        setResult(convertResult);
+      }
+
+      convert();
+    },
+    [amount, from, to]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="container">
+        <input
+          type="text"
+          value={amount}
+          onChange={(e) => handleAmountChange(e)}
+        />
+        <select
+          value={from}
+          onChange={(e) => setFrom(e.target.value)}
+          defaultValue="USD"
         >
-          Learn React
-        </a>
-      </header>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <select
+          value={to}
+          onChange={(e) => setTo(e.target.value)}
+          defaultValue="EUR"
+        >
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+      </div>
+      <p>{result}</p>
     </div>
   );
 }
